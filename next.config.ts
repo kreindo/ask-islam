324deee8e9b76d140c47d1375425e6781fd4ada7
ahmadsan <ahmadsan4p@gmail.com>
2025-03-02 21:51:52 +0800

    Update project configuration and add typecheck script
    - Update Next.js configuration to ignore ESLint and TypeScript build errors
    - Add `typecheck` script to package.json for type checking
    - Update pnpm lockfile to version 9.0

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,19 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     ppr: true,
   },
   images: {
     remotePatterns: [
       {
-        hostname: 'avatar.vercel.sh',
+        hostname: "avatar.vercel.sh",
       },
     ],
   },
